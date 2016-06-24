8d8ef42da7654c35336b872ab91120179c11996d
Dominick <dominickmalzone@aol.com>
2016-06-24 13:36:01 -0700

    hello world

---
+++ script.js
@@ -0,0 +1,10 @@
+var twit = require('twitter');
+	twitter = new twit({
+		consumer_key: '',
+		consumer_secret: ' ',
+		access_token_key: '',
+		access_token_secret: ''
+	});
+
+
+console.log('Hello world');
