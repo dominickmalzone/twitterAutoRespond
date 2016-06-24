5cb63953b250ebce85ead633d6e4b68c2a7cc9d8
Dominick <dominickmalzone@aol.com>
2016-06-24 14:15:25 -0700

    use twitter-node-client instead

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
-var twit = require('twitter');
-	twitter = new twit({
-		consumer_key: '',
-		consumer_secret: ' ',
-		access_token_key: '',
-		access_token_secret: ''
-	});
+var Twitter = require('twitter-node-client').Twitter;
+
+
+  //Callback functions
+    var error = function (err, response, body) {
+        console.log('ERROR [%s]', err);
+    };
+    var success = function (data) {
+        console.log('Data [%s]', data);
+    };
+
+    var Twitter = require('twitter-js-client').Twitter;
+
+    
+    var config = {
+		//secret sauce
+    }
+
+
 
 
 console.log('Hello world');
