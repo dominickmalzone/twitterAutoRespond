acd33b0b2ca37f70e9cd5d1716590b97df98d8c7
Dominick <dominickmalzone@aol.com>
2016-06-24 19:16:07 -0700

    use node-twitterbot instead

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var Twitter = require('twitter-node-client').Twitter;
+var TwitterBot = require('node-twitterbot').TwitterBot;
 
 
   //Callback functions
@@ -9,14 +9,55 @@ var Twitter = require('twitter-node-client').Twitter;
         console.log('Data [%s]', data);
     };
 
-    var Twitter = require('twitter-js-client').Twitter;
+    var Bot = new TwitterBot({
+    	//secret sauce
 
-    
-    var config = {
-		//secret sauce
-    }
+		  });
 
 
 
+    Bot.addAction("tweet", function(twitter, action, tweet) {
+	  Bot.tweet("test tweet");
+	});
 
-console.log('Hello world');
+	Bot.now("tweet");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ //    var params = {
+ //    	q: 'rainbow',
+ //    	count: 2
+ //    }
+
+
+ //    tweetr.getSearch(
+ //    	{'q':' movie -scary :) since:2013-12-27', 'count': 10, 'result\_type':'popular'}
+ //    	, error, success, gotData());
+
+ //    function gotData(err, data, response) {
+ //    	var tweets = data.statuses;
+ //    	for (var i = 0; i < tweets.length; i++) {
+ //    		console.log(tweets[i].text);
+ //    	}
+ //    };
+
+
+	// //twitter.getUserTimeline({ screen_name: 'realDonaldTrump', count: '10'}, error, success);
+
+
+	// //twitter.stream('filter', {track: 'trump'}, function)
+
+	// console.log('Hello world');
