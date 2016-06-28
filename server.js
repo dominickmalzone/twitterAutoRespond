e927e98f8e9e767c66c324f1b58213c2274e97b1
Dominick <dominickmalzone@aol.com>
2016-06-27 17:43:25 -0700

    added respond limit(only once

--- server.js
+++ server.js
@@ -5,43 +5,35 @@ var port = 8300;
 var twitter = require('twitter');
 
 var twit = new twitter({  
-    consumer_key: 'c	2',
-    consumer_secret: 'lm	xr',
-    access_token_key: '16	Qx',
-    access_token_secret: 'Zi	XDZ'
+    consumer_key: 'c 2',
+    consumer_secret: 'l r',
+    access_token_key: '16 x',
+    access_token_secret: 'Zi DZ'
 });
 
 
 var params = {
-    follow: '1666332390' // set user id to who you want to respond to
+    follow: '16 0' // set user id to who you want to respond to
 }
-// 	
-//	
+ 
 
-//	
 
-//var recentId = "";
-//console.log("this is empty id: " + recentId)
-// var responseParams = {
-// 	status: "@dominickmalzone response",
-// 	in_reply_to_status_id: recentId,
-// }
-
+var todaysTweets = [];
+ 
 
 twit.stream('statuses/filter', params,  function(stream) {
   stream.on('data', function(tweet) {
-    //console.log("this is tweet id: " + tweet.id_str);
-    //var recentId = tweet.id_str //set var to tweet you grab from stream
-  
-    	if (tweet.id_str != undefined) { //if it wasnt someone deleteing a a tweet, this was a never ending loop though lol
+      	if (todaysTweets.indexOf(tweet.id_str) > -1) { //Checks to see if it was already responded too
+    		console.log("Responded");
+    	} else { 
+    	console.log("These are todays tweets: " + todaysTweets);
     		twit.post('statuses/update', 
-    			{status: "@dominickmalzone response",
+    			{status: "@dominickmalzone fixed test response",
     			 in_reply_to_status_id: tweet.id_str }
     			, function(error, tweet, response) {
 			  if (!error) {
 			    console.log("This response was sent: " + tweet.text);
-			    tweet.id_str = undefined;
-			    console.log("Set to undefined");
+			    todaysTweets.push(tweet.id_str);
 			  }
 			});
     	};
@@ -52,13 +44,10 @@ twit.stream('statuses/filter', params,  function(stream) {
   });
 });
 
-
-//Just one basic server response, to make sure our server is working.
 app.get('/*', function(req, res){  
-  res.send('Hello Cool World');
+  res.send('Hello World');
 });
 
-//Let's start up our server listening on our port:
 var server = app.listen(port, function(){  
   console.log('Server is listening on port ' + port);
 });
