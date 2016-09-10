14b79103fb80f753e27a3c268c2c59957498f90e
Dominick <dominickmalzone@aol.com>
2016-09-09 22:01:07 -0700

    cleaned up and improved comments

--- server.js
+++ server.js
@@ -5,48 +5,48 @@ var port = 8300;
 var twitter = require('twitter');
 
 var twit = new twitter({  
-    consumer_key: 'c 2',
-    consumer_secret: 'l r',
-    access_token_key: '1 x',
-    access_token_secret: 'Z Z'
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
 });
 
-var params = { follow: '1 ' }// set user id to who you want to respond to
+var params = { follow: '' }// include twitter id of those you want to respond to. (Seperated by commas)
 var todaysTweets = [];
-var data = require('fs').readFileSync('testgif.gif'); //upload image into root dir
+//var data = require('fs').readFileSync('testgif.gif'); //uncomment & upload media into root dir
  
 
 twit.stream('statuses/filter', params,  function(stream) {
   stream.on('data', function(tweet) {
-
+      	
       	if (todaysTweets.indexOf(tweet.id_str) > -1) { //Checks to see if it was already responded too
     		console.log("Responded");
     	} else { 
-    	console.log("These are todays tweets: " + todaysTweets);
+    		if (tweet.user.id_str == ''){ //enter targets twitter id here as well. this makes sure we only respond to the user's tweets (not mentions). 
 
-				//respond with image OR GIF
+				//Media Response
 					twit.post('media/upload', {media: data}, function(error, media, response) {
 					  if (!error) {
 					    console.log("Successful upload"); //console.log(media);
 						    var status = {
-						      status: 'Test gif response',
+						      status: '@userName, this is gif response',
 						      in_reply_to_status_id: tweet.id_str,
 						      media_ids: media.media_id_string // Pass the media id string
 						    }
 					    twit.post('statuses/update', status, function(error, tweet, response) {
 					      if (!error) {
 					        console.log("The response with image was sent: " + tweet.text);
-					        todaysTweets.push(tweet.id_str);
+					        todaysTweets.push(tweet.id_str);// adds to 'todays tweets' so we dont respond multiple times
 					      }
 					    });
 					  }
 					});
-				//end of respond with image
+				//Media response
 
 
-				//respond with normal tweet
+				//Tweet Response
 				   //  		twit.post('statuses/update', 
-				   //  			{status: "@dominickmalzone test embedded vid \n https://www.youtube.com/watch?v=GD4O-YPLcGo",
+				   //  			{status: "@dominickmalzone great script!",
 				   //  			 in_reply_to_status_id: tweet.id_str }
 				   //  			, function(error, tweet, response) {
 							//   if (!error) {
@@ -54,7 +54,9 @@ twit.stream('statuses/filter', params,  function(stream) {
 							//     todaysTweets.push(tweet.id_str);
 							//   }
 							// });
-				//end of normal tweet
+
+				//Tweet Response
+			}
     	};
   });
   stream.on('error', function(error) {
